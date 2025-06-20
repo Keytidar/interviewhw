62c54992819d15c11bdfcc36fe74a206c377fb67
Keytidar <under5frisk@gmail.com>
2025-06-21 02:55:47 +0300

    commit beta

--- index.js
+++ index.js
@@ -1,21 +1,101 @@
-let YearOfBirth = prompt('Enter your year of birth please')?.trim()
-const capitals = ["Kyiv","Washington","London","Paris","Tokyo","Berlin","Moscow","Rome","Beijing","Canberra","Ottawa","Madrid"];
-if (!YearOfBirth) {alert('You cancelled year interview')} else {
-YearOfBirth = Number(YearOfBirth);
-if (!YearOfBirth || YearOfBirth > 2024 || YearOfBirth < 1925) {
-  alert('Error, it is not a proper year')
-} else {
+let YearOfBirth = prompt('Enter your year of birth please')?.trim();
+let YearOfBirthNumber = Number(YearOfBirth);
+let capitalOf = '';
+const currentYear = 2025;
+const capitals = [
+  'Kyiv',
+  'Washington',
+  'London',
+  'Paris',
+  'Tokyo',
+  'Berlin',
+  'Moscow',
+  'Rome',
+  'Beijing',
+  'Canberra',
+  'Ottawa',
+  'Madrid',
+];
 
-  let cityOfLiving = prompt('What city do you live in?')?.trim();
-  if (!cityOfLiving) {alert('You cancelled city interview')} else {
+const sports = [
+  'Football',
+  'Basketball',
+  'Cricket',
+  'Tennis',
+  'Baseball',
+  'Golf',
+  'Boxing',
+  'Table Tennis',
+  'Volleyball',
+  'Rugby',
+  'Hockey',
+  'Formula 1',
+];
 
-  if (isNaN(Number(cityOfLiving)) && String(cityOfLiving).length > 1)  {
-    cityOfLiving = cityOfLiving[0].toUpperCase() + cityOfLiving.slice(1).toLowerCase();
-    alert(capitals.includes(cityOfLiving) ? `You live in capital city ${cityOfLiving}` : `You live in ${cityOfLiving} city`);
-  } else {
-    alert(`${cityOfLiving} is not a proper input`);
+function formatCase(input) {
+  input = input[0].toUpperCase() + input.slice(1).toLowerCase();
+  return input;
+}
+
+function interview() {
+  if (!YearOfBirth) {
+    alert('You cancelled year interview');
+    return;
   }
 
+  if (
+    isNaN(YearOfBirthNumber) ||
+    YearOfBirthNumber >= currentYear ||
+    YearOfBirthNumber < 1925
+  ) {
+    alert('You typed wrong input data');
+    return;
+  }
+  let userAge = currentYear - YearOfBirthNumber;
+
+  let cityOfLiving = prompt('What city do you live in?')?.trim();
+  if (!cityOfLiving) {
+    alert('You cancelled city interview');
+    return;
+  }
+  if (!isNaN(Number(cityOfLiving)) || cityOfLiving.length < 2) {
+    alert('wrong city input');
+    return;
+  }
+
+  cityOfLiving = formatCase(cityOfLiving);
+  if (cityOfLiving === 'Kyiv') {
+    capitalOf = 'Ukraine';
+  }
+  if (cityOfLiving === 'Washington') {
+    capitalOf = 'USA';
+  }
+  if (cityOfLiving === 'London') {
+    capitalOf = 'United Kingdom';
+  }
+
+  let alertMsgCity = capitals.includes(cityOfLiving)
+    ? `You live in capital city ${cityOfLiving}`
+    : `You live in ${cityOfLiving} city`;
+  if (capitalOf) {
+    alertMsgCity = alertMsgCity + `, the capital of ${capitalOf}`;
+  }
+  alert(alertMsgCity);
+  let favSport = prompt('What if your favourite sport?');
+  if (!favSport) {
+    alert('You cancelled sport interview');
+    return;
+  }
+  if (favSport.length > 1 || !isNaN(favSport)) favSport = formatCase(favSport);
+  let AlertMsgSport = sports.includes(favSport)
+    ? `${favSport} ? Good choice`
+    : `${favSport} ? Never heard of it`;
+  alert(AlertMsgSport);
+  let alertMsgFinal = `Your age is ${currentYear - YearOfBirth} years
+${alertMsgCity}
+${favSport} is your favorite sport
+  `;
+  alert(alertMsgFinal);
 }
-}
-}
+
+interview();
